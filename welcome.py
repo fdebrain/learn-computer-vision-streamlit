64a9d905faa487b64857ab66461fcac27c567b6a
fdebrain <12777340+fdebrain@users.noreply.github.com>
2022-09-09 22:57:30 +0200

    refactor: rename main page script

--- welcome.py
+++ welcome.py
@@ -20,9 +20,5 @@ st.markdown(
     - Jump into our [documentation](https://docs.streamlit.io)
     - Ask a question in our [community
         forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
 """
 )

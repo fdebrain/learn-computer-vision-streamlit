b3ec359a78cde45c7c727e297c7b63959dc2838e
fdebrain <12777340+fdebrain@users.noreply.github.com>
2022-11-19 17:21:29 +0100

    refactor(live edge detection): remove redundant code

--- src/live_edge_detection.py
+++ src/live_edge_detection.py
@@ -1,13 +1,5 @@
-from functools import partial
-
 import av
 import cv2
-from aiortc.contrib.media import MediaPlayer
-from streamlit_webrtc import RTCConfiguration, WebRtcMode, webrtc_streamer
-
-RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
-)
 
 
 class VideoProcessorEdgeDetection:
@@ -28,29 +20,3 @@ class VideoProcessorEdgeDetection:
             img = cv2.flip(img, 1)  # Left-Right flip
 
         return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-
-def create_local_file_player(filepath: str):
-    return MediaPlayer(filepath)
-
-
-def create_webcam_stream(processor):
-    return webrtc_streamer(
-        key="webcam",
-        video_processor_factory=processor,
-        rtc_configuration=RTC_CONFIGURATION,
-        media_stream_constraints={"video": True, "audio": False},
-        async_processing=True,
-    )
-
-
-def create_local_video_stream(processor, filepath: str):
-    return webrtc_streamer(
-        key="youtube",
-        video_processor_factory=processor,
-        rtc_configuration=RTC_CONFIGURATION,
-        mode=WebRtcMode.RECVONLY,
-        player_factory=partial(create_local_file_player, filepath=filepath),
-        media_stream_constraints={"video": True, "audio": True},
-        async_processing=True,
-    )
